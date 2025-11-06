c3a1f07dabec710a106749c06d14983b8850be55
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-11-06 12:17:11 +0000

    workshop 002

--- src/counter/script.js
+++ src/counter/script.js
@@ -1,2 +1,15 @@
 // TODO: increment and decrement the count when the buttons are clicked
-
+const decbtn = document.getElementById('decrement'),
+        incbtn = document.querySelector('#increment'),
+        count = document.getElementById('count')
+        console.log(count.innerHTML)
+decbtn.addEventListener('click', () => {
+    let currentValue = parseInt(count.innerHTML)
+    currentValue--
+    count.innerHTML = currentValue
+})
+incbtn.addEventListener('click', () => {
+    let currentValue = parseInt(count.innerHTML)
+    currentValue++
+    count.innerHTML = currentValue
+})
