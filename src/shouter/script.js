e0332b21fe7e85a84263242b21f57f6dc44057fe
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-11-06 12:49:03 +0000

    workshop 004

--- src/shouter/script.js
+++ src/shouter/script.js
@@ -1,2 +1,9 @@
 // TODO: get a reference to the #button and #txt and #output
 // TODO: add an event listener to the #button that changes the text in #output to the uppercase value of #txt
+const btn = document.querySelector('#btn'),
+        out = document.querySelector('#output'),
+        txt = document.querySelector('#txt');
+function toUpperCase() {
+    return out.innerHTML = txt.value.toUpperCase();
+}
+btn.addEventListener('click', toUpperCase)
