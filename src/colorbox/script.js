6d1249d2552acbc1204a04cab4e0719f75dbb652
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-11-06 12:36:37 +0000

    workshop 003

--- src/colorbox/script.js
+++ src/colorbox/script.js
@@ -1,1 +1,15 @@
 // TODO: add an event listener to the .box element that toggles the .red class
+const box = document.querySelectorAll('.box')[0];
+
+// box.onclick = function() {
+//     box.classList.toggle('red')
+// }
+function handleActivation() {
+    box.classList.toggle('red')
+}
+box.addEventListener('click', handleActivation)
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter'){
+        handleActivation()
+    }
+})
